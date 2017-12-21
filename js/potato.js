9ac393fff4139ef6c0a24d2a4832b0e83eddfaa5
JStrohmier <JStrohmier@users.noreply.github.com>
2017-12-21 13:52:27 -0500

    Update potato.js

--- js/potato.js
+++ js/potato.js
@@ -8,11 +8,9 @@ function myFunction() {
         } else {
         txt = "Three people are on the run from the police:" + persona + "," + personb + "," + personc + 
             ". They come across a barn run inside to hide. They see a group of cows, chickens, and a potato sack on the floor." + 
-            persona + "runs to hide with the cows." + personb + "runs to hide with the chickens." + personc + 
-            "runs to hide in the potato sack. The police catch up and come into the barn. They walk towards the cows and ask, 
-            \"Is anybody there?\"" + persona + "goes \"Mooo mooo.\" The police think, 
-    \"Oh, it\'s just the cows. One officer hesitates, knowing she saw someone run in the barn. \"Is anybody there?\" + personb + 
-    "goes, \"cluck cluck cluck.\" The police think, \"Oh, it\'s just the chickens. The same officer hesitates, knowing she definitely saw someone run in the barn. She walks towards the potato sack and asks, \"Is anybody there?\" + 
+            persona + "runs to hide with the cows." + personb + "runs to hide with the chickens." + personc + "runs to hide in the potato sack. The police catch up and come into the barn. They walk towards the cows and ask, \"Is anybody there?\"" + persona + "goes \"Mooo mooo.\" The police think, 
+    \"Oh, it\'s just the cows. One officer hesitates, knowing she saw someone run in the barn. \"Is anybody there?\"" + personb + 
+    "goes, \"cluck cluck cluck.\" The police think, \"Oh, it\'s just the chickens. The same officer hesitates, knowing she definitely saw someone run in the barn. She walks towards the potato sack and asks, \"Is anybody there?\"" + 
             personc + "without missing a beat, goes \"POOOOTAAAAAAATOOOOOEEEEES.\"";
         }
     document.getElementById("demo").innerHTML = txt;
