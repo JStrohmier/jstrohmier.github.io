762aaded5ee4f5d563ada01b46fc11917987df50
JStrohmier <JStrohmier@users.noreply.github.com>
2017-12-21 14:40:15 -0500

    Update potato.js

--- js/potato.js
+++ js/potato.js
@@ -9,12 +9,12 @@ alert('All done. Ready for the message?');
 var sentence = "Three people are on the run from the police: " + persona;
 sentence += ", " + personb;
 sentence += ", " + personc;
-sentence += ". They come across a barn run inside to hide. They see a group of cows, chickens, and a potato sack on the floor. " + persona;
-sentence += " runs to hide with the cows." +personb
-sentence += " runs to hide with the chickens." + personc 
-sentence += " runs to hide in the potato sack. The police catch up and come into the barn. They walk towards the cows and ask, \"Is anybody there?\"" + persona 
+sentence += ". <br>They come across a barn and run inside to hide. They see a group of cows, chickens, and a potato sack on the floor. " + persona;
+sentence += " runs to hide with the cows.<br>" +personb
+sentence += " runs to hide with the chickens.<br>" + personc 
+sentence += " runs to hide in the potato sack. <p>The police catch up and come into the barn. They walk towards the cows and ask, \"Is anybody there?\"" + persona 
 sentence += " goes \"Mooo mooo.\" The police think, \"Oh, it\'s just the cows. One officer hesitates, knowing she saw someone run in the barn. \"Is anybody there?\"" + personb 
 sentence += " goes, \"cluck cluck cluck.\" The police think, \"Oh, it\'s just the chickens. The same officer hesitates, knowing she definitely saw someone run in the barn. She walks towards the potato sack and asks, \"Is anybody there?\"" + 
             personc 
-sentence += " without missing a beat, goes \"POOOOTAAAAAAATOOOOOEEEEES.\"";
+sentence += " without missing a beat, goes \"POOOOTAAAAAAATOOOOOEEEEES.\"<\p>";
 document.write(sentence);
