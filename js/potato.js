799815e3e194f26e6f0df635a22c8837056c5842
JStrohmier <JStrohmier@users.noreply.github.com>
2017-12-21 14:32:49 -0500

    Update potato.js

--- js/potato.js
+++ js/potato.js
@@ -10,11 +10,11 @@ var sentence = "Three people are on the run from the police: " + persona;
 sentence += ", " + personb;
 sentence += ", " + personc;
 sentence += ". They come across a barn run inside to hide. They see a group of cows, chickens, and a potato sack on the floor. " + persona;
-sentence += "runs to hide with the cows." +personb
-sentence += "runs to hide with the chickens." + personc 
-sentence += "runs to hide in the potato sack. The police catch up and come into the barn. They walk towards the cows and ask, \"Is anybody there?\"" + persona 
-sentence += "goes \"Mooo mooo.\" The police think, \"Oh, it\'s just the cows. One officer hesitates, knowing she saw someone run in the barn. \"Is anybody there?\"" + personb 
-sentence += "goes, \"cluck cluck cluck.\" The police think, \"Oh, it\'s just the chickens. The same officer hesitates, knowing she definitely saw someone run in the barn. She walks towards the potato sack and asks, \"Is anybody there?\"" + 
+sentence += " runs to hide with the cows." +personb
+sentence += " runs to hide with the chickens." + personc 
+sentence += " runs to hide in the potato sack. The police catch up and come into the barn. They walk towards the cows and ask, \"Is anybody there?\"" + persona 
+sentence += " goes \"Mooo mooo.\" The police think, \"Oh, it\'s just the cows. One officer hesitates, knowing she saw someone run in the barn. \"Is anybody there?\"" + personb 
+sentence += " goes, \"cluck cluck cluck.\" The police think, \"Oh, it\'s just the chickens. The same officer hesitates, knowing she definitely saw someone run in the barn. She walks towards the potato sack and asks, \"Is anybody there?\"" + 
             personc 
-sentence += "without missing a beat, goes \"POOOOTAAAAAAATOOOOOEEEEES.\"";
+sentence += " without missing a beat, goes \"POOOOTAAAAAAATOOOOOEEEEES.\"";
 document.write(sentence);
